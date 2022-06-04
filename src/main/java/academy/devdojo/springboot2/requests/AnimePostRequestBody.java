ff84a412a3ce055c81073f4a10c284b70caf79bd
ana004 <paulaana08002019@gmail.com>
2022-06-04 12:04:04 -0300

    Adicionada sobrescricao handler do Spring

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -1,6 +1,7 @@
 package academy.devdojo.springboot2.requests;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -9,4 +10,6 @@ import javax.validation.constraints.NotNull;
 public class AnimePostRequestBody {
     @NotEmpty(message = "The anime name cannot be empty")
     private String name;
+    @URL(message = "The URL is not valid")
+    private String url;
 }
